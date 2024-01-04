e1534b5628f1c8fbda44ac58aa80a73bf38b8843
DongHyeok Lim <jh07050@gmail.com>
2024-01-04 20:46:58 +0900

    refactor: username unique add
    username에 unique 옵션을 추가합니다.

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -2,5 +2,8 @@ import * as mongoose from 'mongoose';
 
 // 240104 ldhbenecia || 객체 리터럴 사용
 export const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  chatRooms: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ChatRoom' }],
 });
+
+export const User = mongoose.model('User', UserSchema);
