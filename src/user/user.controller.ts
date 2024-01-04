9bfe8e0579493707716ef223eb09b40278d66a7a
DongHyeok Lim <jh07050@gmail.com>
2024-01-04 19:10:40 +0900

    refactor: async Promise 반환 타입 지정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './interface/user.interface';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('/register')
-  async create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.create(createUserDto);
   }
 }
