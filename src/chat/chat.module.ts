7678252aff343a640e81869295c40ed6d73c8aea
DongHyeok Lim <jh07050@gmail.com>
2024-01-05 00:55:31 +0900

    feat: chat module provider 추가

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatMessageSchema, ChatRoomSchema } from './schema';
+import { ChatRoomService } from './chat-room/chat-room.service';
+import { ChatRoomRepository } from './chat-room/chat-room.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'ChatRoom', schema: ChatRoomSchema }]),
     MongooseModule.forFeature([{ name: 'ChatMessage', schema: ChatMessageSchema }]),
   ],
-  providers: [ChatGateway],
+  providers: [ChatGateway, ChatRoomService, ChatRoomRepository],
 })
 export class ChatModule {}
