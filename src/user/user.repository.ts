7536f75baa82ad009c8cdf598fdbcf61ac823c13
DongHyeok Lim <jh07050@gmail.com>
2024-01-04 19:10:23 +0900

    fix: user.repository 의존성 주입 수정

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -2,7 +2,9 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './interface/user.interface';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class UserRepository {
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
