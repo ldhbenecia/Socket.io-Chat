d60be6e82ce90630b76988dab893016a74f57f07
DongHyeok Lim <jh07050@gmail.com>
2024-01-07 22:29:10 +0900

    feat: chat.module에 chatMessage 메소드 추가
    chat.module에 chatMessage 메소드를 provider에 추가합니다.

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,12 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ChatMessageSchema, ChatRoomSchema } from './schema';
 import { ChatRoomService } from './chat-room/chat-room.service';
 import { ChatRoomRepository } from './chat-room/chat-room.repository';
+import { ChatMessageRepository } from './chat-message/chat-message.repository';
+import { ChatMessageService } from './chat-message/chat-message.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'ChatRoom', schema: ChatRoomSchema }]),
     MongooseModule.forFeature([{ name: 'ChatMessage', schema: ChatMessageSchema }]),
   ],
-  providers: [ChatGateway, ChatRoomService, ChatRoomRepository],
+  providers: [ChatGateway, ChatRoomService, ChatRoomRepository, ChatMessageService, ChatMessageRepository],
 })
 export class ChatModule {}
