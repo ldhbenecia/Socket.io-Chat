4bc1193870be0e2612f960c205051d537f256db0
DongHyeok Lim <jh07050@gmail.com>
2024-01-08 18:52:49 +0900

    feat: 1:1 채팅방 생성 기능 구현
    1:1 채팅방 생성 기능을 구현하고 DB에 채팅방을 기록합니다.

--- src/chat/chat-room/chat-room.repository.ts
+++ src/chat/chat-room/chat-room.repository.ts
@@ -2,13 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { ChatRoom } from '../schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Types } from 'mongoose';
 
 @Injectable()
 export class ChatRoomRepository {
   constructor(@InjectModel(ChatRoom.name) private chatRoomModel: Model<ChatRoom>) {}
 
-  async createChatRoom(name: string, participants: string[]): Promise<ChatRoom> {
-    const room = new this.chatRoomModel({ name, participants });
+  // async createChatRoom(name: string, participants: string[]): Promise<ChatRoom> {
+  //   const participantIds = participants.map((userId) => new Types.ObjectId(userId));
+
+  //   const room = new this.chatRoomModel({ name, participants: participantIds });
+  //   return await room.save();
+  // }
+
+  async createPrivateChatRoom(userId: string, roomName: string): Promise<ChatRoom> {
+    const participantIds = [new Types.ObjectId(userId)];
+
+    const room = new this.chatRoomModel({ name: roomName, participants: participantIds });
     return await room.save();
   }
 }
--- src/chat/chat-room/chat-room.service.ts
+++ src/chat/chat-room/chat-room.service.ts
@@ -6,7 +6,13 @@ import { ChatRoom } from '../schema';
 export class ChatRoomService {
   constructor(private chatRoomRepository: ChatRoomRepository) {}
 
-  async createChatRoom(name: string, participants: string[]): Promise<ChatRoom> {
-    return this.chatRoomRepository.createChatRoom(name, participants);
+  // 단체 채팅방 생성
+  // async createChatRoom(name: string, participants: string[]): Promise<ChatRoom> {
+  //   return this.chatRoomRepository.createChatRoom(name, participants);
+  // }
+
+  // 1:1 채팅방 생성
+  async createPrivateChatRoom(userId: string, roomName: string): Promise<ChatRoom> {
+    return await this.chatRoomRepository.createPrivateChatRoom(userId, roomName);
   }
 }
