aa04610940b91efcd6eebeadbc47e67a03dda58d
DongHyeok Lim <jh07050@gmail.com>
2024-01-09 20:33:53 +0900

    feat: 이미 채팅방에 참여한 경우 중복 참여 불가능 구현
    이미 채팅방에 참여했는데 중복으로 요청을 보내는 경우를 처리합니다.

--- src/chat/chat-room/chat-room.repository.ts
+++ src/chat/chat-room/chat-room.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { ChatRoom } from '../schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -17,7 +17,11 @@ export class ChatRoomRepository {
   async joinChatRoom(userId: string, roomName: string): Promise<ChatRoom> {
     const existingRoom = await this.chatRoomModel.findOne({ roomName });
     if (!existingRoom) {
-      throw new Error(`Chat room with name ${roomName} does not exist.`);
+      throw new ForbiddenException(`Chat room with name ${roomName} does not exist.`);
+    }
+
+    if (existingRoom.participants.includes(userId)) {
+      throw new ForbiddenException(`User with ID ${userId} is already in the chat room.`);
     }
 
     existingRoom.participants.push(userId);
