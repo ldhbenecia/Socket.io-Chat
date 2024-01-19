b99fb9e11ee9f805b1b5f4776accfc3be1c1533a
DongHyeok Lim <jh07050@gmail.com>
2024-01-19 20:02:20 +0900

    feat: chatMessage가 chatRoom을 참조하도록 스키마 변경

--- src/chat/schema/chat-message.schema.ts
+++ src/chat/schema/chat-message.schema.ts
@@ -1,13 +1,17 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument, Types } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { ChatRoom } from './chat-room.schema';
 
 export type ChatMessageDocument = HydratedDocument<ChatMessage>;
 
 // 240104 ldhbenecia || 클래스 사용
 @Schema({ collection: 'chatmessages' })
 export class ChatMessage {
-  @Prop({ type: Types.ObjectId, ref: 'ChatRoom' })
-  roomName: Types.ObjectId;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'ChatRoom' })
+  roomId: ChatRoom;
+
+  @Prop({ type: String })
+  roomName: string;
 
   @Prop({ type: String, ref: 'User' })
   senderId: string;
