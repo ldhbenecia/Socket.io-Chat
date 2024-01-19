d8810204e22802cd67698455e1dab609e09b76be
DongHyeok Lim <jh07050@gmail.com>
2024-01-19 19:17:31 +0900

    feat: 생성되지 않은 채팅방에 메세지 전송 오류 처리
    생성되지 않은 채팅방에 메세지를 전송하는 것에 대한 오류처리를 진행합니다.

--- src/chat/chat-message/chat-message.repository.ts
+++ src/chat/chat-message/chat-message.repository.ts
@@ -1,13 +1,22 @@
-import { Injectable } from '@nestjs/common';
-import { ChatMessage } from '../schema';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ChatMessage, ChatRoom } from '../schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class ChatMessageRepository {
-  constructor(@InjectModel(ChatMessage.name) private chatMessageModel: Model<ChatMessage>) {}
+  constructor(
+    @InjectModel(ChatMessage.name) private chatMessageModel: Model<ChatMessage>,
+    @InjectModel(ChatRoom.name) private chatRoomModel: Model<ChatRoom>,
+  ) {}
 
   async saveMessage(roomName: string, senderId: string, content: string): Promise<ChatMessage> {
+    const existingRoom = await this.chatRoomModel.findOne({ roomName }).exec();
+
+    if (!existingRoom) {
+      throw new NotFoundException(`Chat room '${roomName}' not found`);
+    }
+
     const message = new this.chatMessageModel({ roomName, senderId, content });
     return await message.save();
   }
